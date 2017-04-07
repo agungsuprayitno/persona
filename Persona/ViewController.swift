611b71d73d8617f34982a3648891a1e281afd181
Agung Suprayitno <agung.suprayitno92@gmail.com>
2017-04-07 14:50:18 +0700

    Initial Commit

---
+++ Persona/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Persona
+//
+//  Created by Agung Suprayitno on 4/7/17.
+//  Copyright © 2017 Agung Suprayitno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
